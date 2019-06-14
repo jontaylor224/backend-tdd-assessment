db30d78375d140608466040c2f63621f94abb78a
Jon Taylor <jontaylor224@gmail.com>
2019-06-14 13:14:40 -0500

    fixed tests, added echo code

--- echo.py
+++ echo.py
@@ -2,21 +2,45 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility"""
 
-__author__ = "???"
+__author__ = "jontaylor224"
 
 
 import sys
+import argparse
 
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    pass
+    parser = argparse.ArgumentParser(
+        description='Perform transformation on input text.')
+    parser.add_argument(
+        '-u', '--upper', help='convert text to uppercase', action='store_true')
+    parser.add_argument(
+        '-l', '--lower', help='convert text to lowercase', action='store_true')
+    parser.add_argument(
+        '-t', '--title', help='convert text to titlecase', action='store_true')
+    parser.add_argument('text', help='text to be manipulated')
+    return parser
 
 
 def main(args):
     """Implementation of echo"""
-    pass
+    parser = create_parser()
+    ns = parser.parse_args(args)
+
+    text = ns.text
+
+    if ns.upper:
+        text = text.upper()
+
+    if ns.lower:
+        text = text.lower()
+
+    if ns.title:
+        text = text.title()
+
+    return text
 
 
 if __name__ == '__main__':
-    pass
+    print(main(sys.argv[1:]))
